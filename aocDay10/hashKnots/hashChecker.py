3b054214d3e615179c5188817cebbbd97d53c09d
Darrin Giesy <zog@softwareunseen.com>
2017-12-10 19:59:41 -0600

    Day 10 initial code drop

---
+++ aocDay10/hashKnots/hashChecker.py
@@ -0,0 +1,62 @@
+'''
+Created on Dec 10, 2017
+
+@author: Zog
+'''
+def hash_checker(inputString):
+    baseChars = []
+    for i in inputString:
+        baseChars.append(ord(i))
+    part2Appendix = [17, 31, 73, 47, 23]
+    workingLengths = baseChars + part2Appendix
+    skipSpan = 0
+    listPosition = 0
+    numList = list(range(256))
+    sublist = []
+    
+    workingList = numList
+    listLen = len(workingList)
+    
+    for x in range(64):
+        for i in workingLengths:
+            if listPosition + i > listLen:
+                subListIdices = list(range(listPosition%listLen, listLen))
+                for k in range((listPosition+i)%listLen):
+                    subListIdices.append(k)
+            else:
+                subListIdices = list(range(listPosition, listPosition + i))
+            for j in subListIdices:
+                index = j%listLen
+                sublist.append(workingList[index])
+            for j in subListIdices:
+                workingList[j] = sublist.pop()
+            listPosition = (listPosition + i + skipSpan)%listLen
+            skipSpan = skipSpan + 1
+    
+    rawDenseHash = []
+    for i in range(16):
+        denseEntry = 0
+        for j in range(16):
+            base = i * 16
+            index = j + base
+            denseEntry = denseEntry ^ workingList[index]
+        rawDenseHash.append(denseEntry)
+    
+    hexDenseHash = ""
+    for i in rawDenseHash:
+        hexString = format(i,'x')
+        if len(hexString) == 1:
+            hexString = '0' + hexString
+        hexDenseHash = hexDenseHash + hexString                
+    
+    print("Final hex hash: " + hexDenseHash)
+        
+    checksum = workingList[0] * workingList[1]
+    print("Product of first two elements: " + str(checksum))
+    return hexDenseHash
+
+if __name__ == '__main__':
+    baseLengthsChars = "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
+    hash_checker(baseLengthsChars)
+    
+    pass
---
+++ aocDay10/hashKnots/hashCheckerUnitTest.py
@@ -0,0 +1,25 @@
+'''
+Created on Dec 10, 2017
+
+@author: Zog
+'''
+import unittest
+from hashKnots.hashChecker import hash_checker
+
+
+class Test(unittest.TestCase):
+
+
+    def testHashChecker(self):
+        testStrings = ["", "AoC 2017", "1,2,3", "1,2,4"]
+        testExpectedAnswers = ["a2582a3a0e66e6e86e3812dcb672a272", "33efeb34ea91902bb2f59c9920caa6cd", "3efbe78a8d82f29979031a4aa0b16a9d", "63960835bcdc130f0b66d7ff4f6a5a8e"]
+        
+        for i in range(len(testStrings)):
+            assert(testExpectedAnswers[i] == hash_checker(testStrings[i]))
+        
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testHashChecker']
+    unittest.main()
