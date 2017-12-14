eae36625e423397c5edea660d4fe4c5c08eb8223
Darrin Giesy <zog@softwareunseen.com>
2017-12-14 09:27:12 -0600

    Day 13 part 1 code drop. Part 2 pending

--- aocDay13/packageScanner/damageCounter.py
+++ aocDay13/packageScanner/damageCounter.py
@@ -13,16 +13,47 @@ def read_data(fileName):
             walls[cleanContent[0]]=cleanContent[1].strip()
     return walls
 
+def populate_data(fw2):
+    expandedFireWalls = {}
+    count = 0
+    while len(fw2) > 0:
+        keyList = list(fw2.keys())
+#         print(str(keyList))
+        if str(count) in keyList:
+            expandedFireWalls[count] = fw2[str(count)]
+            del(fw2[str(count)])
+            count += 1
+        else:
+            expandedFireWalls[count] = None
+            count += 1
+
+    return expandedFireWalls
+
+def find_location_cost(maxDepth, moves):
+    cost = 0
+    bottomTier = maxDepth - 1
+    if moves % bottomTier == 0:
+        if (moves / bottomTier) % 2 == 0:
+            cost = moves * maxDepth
+    return cost
 
 if __name__ == '__main__':
-    fileName = "../compressedFirewalls.txt"
+    fileName = "../firewalls.txt"
     
     compressedFirewalls = read_data(fileName)
 #     Create a disposable copy
-    fw2 = dict(compressedFirewalls)
+    disposableWalls = dict(compressedFirewalls)
+    fullWalls = populate_data(disposableWalls)
     
-    while len(fw2) > 0:
-        break
+    picos = len(fullWalls)
+    step = 0
+    totalCost = 0
+    caughtAt = []
+    while step < picos:
+        if not fullWalls[step] == None:
+            currentDepth = int(fullWalls[step])
+            totalCost += find_location_cost(currentDepth,step)
+        step += 1
     
-    
+    print("Total cost for Part 1: " + str(totalCost))
     pass
--- aocDay13/packageScanner/damageCounterUnitTest.py
+++ aocDay13/packageScanner/damageCounterUnitTest.py
@@ -4,12 +4,18 @@ Created on Dec 13, 2017
 @author: dgiesy
 '''
 import unittest
+from damageCounter import find_location_cost
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
+    def testFindScanLocation(self):
+        assert(find_location_cost(3, 0) == 0)
+        assert(find_location_cost(4, 4) == 0)
+        assert(find_location_cost(4, 6) == 24)
+        assert(find_location_cost(12, 44) == 528)
+        assert(find_location_cost(8, 52) == 0)
         pass
 
 
